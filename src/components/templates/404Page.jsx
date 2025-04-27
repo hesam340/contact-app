3e941b020a4fcf292bad875eaf3ae934cf787032
hesam khaki <hesamkhaki340@gmail.com>
2025-04-27 17:48:01 +0330

    404 page is added and some styles are handled

--- src/components/templates/404Page.jsx
+++ src/components/templates/404Page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./PageNotFound.module.css";
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className={styles.container}>
+      <img src="./404.webp" alt="404" />
+      <div>
+        <p>صفحه مورد نظر شما یافت نشد !</p>
+        <Link to="/">برگشت به صفحه اصلی</Link>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
